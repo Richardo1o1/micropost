e219f47179f6f5e7eb0daac075adc98b8049fa9b
Richard <richard119@gmail.com>
2024-06-16 12:36:28 +0800

    Build fix: dynamic server usage

--- src/app/api/current/route.ts
+++ src/app/api/current/route.ts
@@ -1,10 +1,27 @@
-import { NextResponse } from "next/server";
+export const dynamic = "force-dynamic";
 
-import serverAuth from "@/libs/serverAuth";
+import { NextResponse } from "next/server";
+import { options } from "@/app/api/auth/[...nextauth]/options"
+import { getServerSession } from "next-auth/next"
+
+import prisma from "@/libs/prismadb"
+
 // Process POST request to /api/current
 export async function GET(request : Request) {
   try{
-    const { currentUser } = await serverAuth();
+    const session = await getServerSession(options);
+
+    if(!session?.user?.email){
+      //ignore not signed in error
+      //throw new Error('Not signed in with a user account');
+      return NextResponse.json( null , { status: 200 });
+    }
+
+    const currentUser = await prisma.user.findUnique({
+      where: {
+          email: session.user.email
+      }
+    });
 
     if (!currentUser) {
       return NextResponse.json( null , { status: 200 });
